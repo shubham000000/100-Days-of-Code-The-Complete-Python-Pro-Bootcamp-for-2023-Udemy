4e8d6e0ade6ad732fbf28ddc8a930ed2337950c0
Shubham Borghare <borghare.sb@gmail.com>
2023-03-08 14:33:19 +0900

    Create main.py

---
+++ Day 2/Exercise 1 - Data Types/main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#new_two_digit_number = int(two_digit_number)
+num_1 = two_digit_number[0]
+num_2 = two_digit_number[1]
+
+total = int(num_1) + int(num_2)
+
+print(total)
