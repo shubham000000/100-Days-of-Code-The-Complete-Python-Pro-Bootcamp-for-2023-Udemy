59cace08a291cd383b5880f0835963dde0a13a45
Shubham Borghare <borghare.sb@gmail.com>
2023-03-08 14:48:50 +0900

    Update main.py

--- Day 3/Exercise 1 - Odd or Even/main.py
+++ Day 3/Exercise 1 - Odd or Even/main.py
@@ -3,10 +3,15 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
-
-
 if(number % 2 != 0):
     print("This is an odd number.")
 else:
     print("This is an even number.")
+    
+
+#Ticketing Flow Chart Version 2
+#https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Rollercoaster%202#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1J7_rw1flGeF0hmc_zrMzPX7t6xkbcsiX%26export%3Ddownload
+
+#Ticketing Flow Chart Version 3
+#https://app.diagrams.net/?lightbox=1&target=blank&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Rollercoaster%202#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1XaUDMIKOxCUzJbsuZevgHZmgKr7rICbI%26export%3Ddownload
+
