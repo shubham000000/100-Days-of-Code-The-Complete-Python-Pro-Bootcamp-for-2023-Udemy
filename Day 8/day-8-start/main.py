2b0bbe38f946916b8c79434a4390913cb63530e2
Shubham Borghare <borghare.sb@gmail.com>
2023-03-16 14:01:33 +0900

    Create main.py

---
+++ Day 8/day-8-start/main.py
@@ -0,0 +1,34 @@
+# Review: 
+# Create a function called greet().
+# def greet():
+# # Write 3 print statements inside the function.
+#   print("Welcome to Dreamland")
+#   print("Hello")
+#   print("Greetings!")
+
+# # Call the greet() function and run your code.
+# greet()
+
+
+# def greet_with_name(name):
+# # Write 3 print statements inside the function.
+#   print("Welcome to Dreamland")
+#   print(f"Hello {name}")
+#   print("Greetings!")
+
+# # Call the greet() function and run your code.
+# greet_with_name("gaurav")
+
+# def greet_with(name, location):
+#   print(f"Hello {name}")
+#   print(f"What is it like in {location}?")
+
+# greet_with("Shubham", "Nowhere University")
+
+#Function with keyword arguments
+def greet_with_key(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+greet_with_key(location ="Nowhere University", name="Shubham")
+
